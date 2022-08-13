6c689011d07dea0dd09ee6ceb8a7ac203e65b3cb
chengdengwei <chengdengwei>
2022-08-13 09:35:40 +0800

    两数相加

--- app/src/test/java/demo/adan/com/myapplication/Leetcode/awei/Leetcode2.java
+++ app/src/test/java/demo/adan/com/myapplication/Leetcode/awei/Leetcode2.java
@@ -33,8 +33,8 @@ public class Leetcode2 {
 //        n1.next.previous = n1;
 
 
-        ListNode l1 = new ListNode(2,new ListNode(4,new ListNode(3)));
-        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode l1 = new ListNode(9, new ListNode(9, new ListNode(1)));
+        ListNode l2 = new ListNode(1);
         ListNode result = addTwoNumbers(l1,l2);
 
         while (result != null ){
@@ -44,9 +44,70 @@ public class Leetcode2 {
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode r = null;
-        int a = 876;
+        ListNode r = new ListNode(0,null);
+        ListNode res = r;
+        int y = 0;
+        boolean over = false;
+        while (!over) {
+            if (l1 != null && l2 != null) {
+                int t = l1.val + l2.val + y;
+                if (t >= 10) {
+                    y = 1;
+                    r.val = t % 10;
+                    r.next = new ListNode(y,null);
+                }else {
+                    y = 0;
+                    r.val = t;
+                    r.next = new ListNode(y,null);
+                }
+            }else  {
+                if (l1 == null && l2 == null) {
+                    if (y == 1) {
+                        r.val = y;
+                        r.next = null;
+                        y = 0;
+                    }
+                }else  {
+                    if (l1 != null) {
+                        int t = l1.val + y;
+                        if (t >= 10) {
+                            y = 1;
+                            r.val = t % 10;
+                            r.next = new ListNode(y,null);
+                        }else {
+                            r.val = l1.val + y;
+                            r.next = l1.next;
+                            y = 0;
+                        }
+                    }
+                    if (l2 != null) {
+                        int t = l2.val + y;
+                        if (t >= 10) {
+                            y = 1;
+                            r.val = t % 10;
+                            r.next = new ListNode(y,null);
+                        }else {
+                            r.val = l2.val + y;
+                            r.next = l2.next;
+                            y = 0;
+                        }
+                    }
+                }
+            }
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+            if (l1 == null && l2 == null && y == 0) {
+                over = true;
+                r.next = null;
+            }else {
+                r = r.next;
+            }
+        }
 
-        return r;
+        return res;
     }
 }
