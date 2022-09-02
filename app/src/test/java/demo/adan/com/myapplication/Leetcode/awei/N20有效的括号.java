0daf0c9dc6a4013c10131770c100ce4ae246abb2
chengdengwei <chengdengwei>
2022-09-02 08:55:31 +0800

    update

--- app/src/test/java/demo/adan/com/myapplication/Leetcode/awei/N20有效的括号.java
+++ app/src/test/java/demo/adan/com/myapplication/Leetcode/awei/N20有效的括号.java
@@ -5,21 +5,32 @@ import org.junit.Test;
 public class N20有效的括号 {
     @Test
     public void main() {
-        boolean r = isValid("()");
+        boolean r = isValid("()[]{}");
         System.out.println(r);
     }
 
     public boolean isValid(String s) {
-        String t = s;
+        String t = "";
         for (int i = 0; i < s.length(); i++) {
-            for (int j = i+1; j < s.length(); j++) {
-                if (s.charAt(i) == '(') {
-                    if (s.charAt(j) == ')') {
-//                        t.
-                    }
+            if (t.length() > 0) {
+                int l = s.indexOf(t) - 1;
+                int r = l + t.length() + 1;
+                if ((s.charAt(r) == ')' && s.charAt(l) == '(') ||
+                        s.charAt(r) == ']' && s.charAt(l) == '[' ||
+                        s.charAt(r) == '}' && s.charAt(l) == '{') {
+                    t = s.substring(l,r+1);
+                }
+            }else  {
+                if ((s.charAt(i) == ')' && s.charAt(i-1) == '(') ||
+                        s.charAt(i) == ']' && s.charAt(i-1) == '[' ||
+                        s.charAt(i) == '}' && s.charAt(i-1) == '{') {
+                    t = s.substring(i-1,i+1);
                 }
             }
         }
-        return true;
+        if (t.equals(s)) {
+            return true;
+        }
+        return false;
     }
 }
